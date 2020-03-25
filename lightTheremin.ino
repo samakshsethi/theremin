f51741f9cbf70986ec4d61b8c2b05d20d0cb5e46
samakshsethi <62542213+samakshsethi@users.noreply.github.com>
2020-03-25 20:00:57 +0530

    Add files via upload

--- lightTheremin.ino
+++ lightTheremin.ino
@@ -1,3 +1,15 @@
+/* Theremin using photoresistor
+ * By Samaksh Sethi
+ * 
+ * Execution procedure:
+ * 1. The code goes into calibration mode and senses the range of values that will be possible and defines a range accordingly
+ * 2. The led will turn off and the theremin is ready to be used, along with the pitch of the buzzer, the brightness of led changes too 
+ * 
+ * NOTE- Do NOT use the pin #3 and #11 for analogWrite() (ie the led) because the PWM interferes with the timer of the tone() function!
+ * 
+ * GitHub link - https://github.com/samakshsethi/theremin
+ */
+ 
 int reading; 
 int low = 1023;
 int high = 0;
